44a8071cbdb7e516c5087fd7121c7e68c0675161
Denis <denislav.zaimov@gmail.com>
2020-01-22 23:08:43 -0500

    fixed the spasing between the project cards and cleaned up the index.blade

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,9 +1,3 @@
-{{-- @php
-    $numOfCols = 3;
-    $rowCount = 0;
-    $bootstrapColWidth = 12 / $numOfCols;
-@endphp --}}
-
 @extends('layouts.app')
 @section('content')
     <main class="projects">
@@ -15,23 +9,21 @@
         <div class="mb-3 projects__wrapper">
             <div class="row">
                 @forelse ($projects as $project)
-                    <div class="card col-md-6 col-lg-4 mb-4 shadow-sm projects__card">
-                        <div class="card-header">
-                            <h4 class="my-0 projects__title">
-                                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-                            </h4>
-                        </div>
-                        <div class="card-body">
-                            {{ Str::limit($project->description, 200) }}
+                    <div class="col-md-6 col-lg-4 mb-4">
+                        <div class="card shadow-sm projects__card">
+                            <div class="card-header">
+                                <h4 class="my-0 projects__title">
+                                    <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                {{ Str::limit($project->description, 200) }}
+                            </div>
                         </div>
                     </div>
-                {{-- @if($loop->index > 1 && $rowCount % $numOfCols == 0)
-                    <div class="w-100"></div>
-                @endif --}}
-                
-            @empty
-                <p>No projects</p>
-            @endforelse
+                @empty
+                    <p>No projects</p>
+                @endforelse
             </div>
         </div>
     </main>
