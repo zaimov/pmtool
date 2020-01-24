5fdab92788d1ceac96c9aadac86038e7672cf638
Denis <denislav.zaimov@gmail.com>
2020-01-23 21:19:12 -0500

    started styling the single project view and created ProjectTasksController

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.app')
 @section('content')
-    <main class="projects">
-        <div class="d-flex justify-content-between mb-3 projects__header">
-            <p>My Projects</p>
-            <a href="/projects/create" class="btn btn-primary">New Project</a>
+    <main class="projects global">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex justify-content-between mb-3">
+                    <h2>My Projects</h2>
+                    <a href="/projects/create" class="btn btn-primary">New Project</a>
+                </div>
+            </div>
         </div>
     
-        <div class="mb-3 projects__wrapper">
+        <section class="mb-3 projects__wrapper">
             <div class="row">
                 @forelse ($projects as $project)
                     <div class="col-md-6 col-lg-4 mb-4">
-                        <div class="card shadow-sm projects__card">
+                        <div class="card shadow-sm global__card projects__card">
                             <div class="card-header">
                                 <h4 class="my-0 projects__title">
                                     <a href="{{ $project->path() }}">{{ $project->title }}</a>
@@ -25,6 +29,6 @@
                     <p>No projects</p>
                 @endforelse
             </div>
-        </div>
+        </section>
     </main>
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,6 +1,52 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <a href="/projects">Go Back</a>
+    <main class="project global">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex justify-content-between mb-3">
+                    <p>
+                        <a href="/projects">My Projects</a> / {{ $project->title }}
+                    </p>
+                    <a href="/projects/create" class="btn btn-primary">New Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="project__tasks-wrapper mb-3">
+                    <h2 class="mb-3">Tasks</h2>
+                    <div class="project__task card mb-2 p-2">
+                        Task 1
+                    </div>
+                    <div class="project__task card mb-2 p-2">
+                        Task 2
+                    </div>
+                    <div class="project__task card mb-2 p-2">
+                        Task 3
+                    </div>
+                </div>
+
+                <div class="gnotes mb-3">
+                    <h2 class="mb-3">General notes</h2>
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Lorem ipsum</textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card shadow-sm global__card">
+                    <div class="card-header">
+                        <h4 class="my-0 projects__title">
+                            <h4>{{ $project->title }}</h4>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        {{ Str::limit($project->description, 200) }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </main>
 @endsection
